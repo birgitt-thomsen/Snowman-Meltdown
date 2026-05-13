9572ef415186619244be1207f8ee38ded4946ad1
Birgitt Thomsen <birgitt.thomsen@t-online.de>
2026-05-13 22:00:09 +0200

    Expanded snowman melting stages for smoother transition.

--- ascii_art.py
+++ ascii_art.py
@@ -3,28 +3,48 @@
 STAGES = [
      # Stage 0: Full snowman
      """
-      ___  
-     /___\\ 
-     (o o) 
-     ( : ) 
-     ( : ) 
+      ___
+    _|___|_
+     (o o)
+  `--( : )--´
+    (  :  )
      """,
      # Stage 1: Bottom part starts melting
      """
-      ___  
-     /___\\ 
-     (o o) 
-     ( : ) 
+      ___
+    _|___|_
+     (o o)
+  `--( : )--´
+ __(   :   )__
      """,
-     # Stage 2: Only the head remains
+     # Stage 2: Top part starts sagging
      """
-      ___  
-     /___\\ 
-     (o o) 
+      ___
+    _|___|_
+     (- -)
+  _-(  .  )-_
+__(    .    )__
      """,
-     # Stage 3: Snowman completely melted
+     # Stage 3: Top part is gone
      """
-      ___  
-     /___\\ 
+     
+      ___
+    _|___|_
+      (.)
+ ___(  .  )___
+     """,
+    # Stage 4: Only a puddle left
+     """
+     
+      ___
+    _|___|_
+      .-.
+  ___(   )___
+     """,
+    # Stage 5: Only the hat left
+     """
+      ___
+    _|___|_
+  ____.-.____
      """
  ]
